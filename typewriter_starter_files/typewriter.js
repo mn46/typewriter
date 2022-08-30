a0177971f27c4f29925085c0dde0d3892b56da67
Marta <m.nowak033@gmail.com>
2022-08-30 15:37:27 +0200

    managed to make sound play when letters appear

--- typewriter_starter_files/typewriter.js
+++ typewriter_starter_files/typewriter.js
@@ -1,22 +1,9 @@
 "use strict";
 
-// let original = document.querySelector("#typewriter").textContent;
-// let counter = 0;
-// let animated;
-
-// loop();
-
-// function loop() {
-//   if (counter < original.length) {
-//     animated = original.slice(0, counter);
-//     document.querySelector("#animated").textContent = animated;
-//     counter++;
-//   }
-//   setTimeout(loop, 500);
-// }
-
 let original = document.querySelector("#typewriter").textContent;
 let counter = 0;
+const typesound1 = document.querySelector("#typekey1");
+// let randomSound = Math.floor(Math.random() * 2);
 
 loop();
 
@@ -24,6 +11,11 @@ function loop() {
   if (counter < original.length) {
     document.querySelector("#typewriter").textContent = original.slice(0, counter);
     counter++;
+    playSound();
     setTimeout(loop, 500);
   }
+}
+
+function playSound() {
+  typesound1.play();
 }
