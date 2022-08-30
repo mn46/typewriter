9589f46c1e41ea33fd8cdae63e6a8f4488d33019
Marta <m.nowak033@gmail.com>
2022-08-30 15:58:36 +0200

    made the space sound work

--- typewriter_starter_files/typewriter.js
+++ typewriter_starter_files/typewriter.js
@@ -3,19 +3,29 @@
 let original = document.querySelector("#typewriter").textContent;
 let counter = 0;
 const typesound1 = document.querySelector("#typekey1");
+const typesound2 = document.querySelector("#typekey2");
+const spacesound = document.querySelector("#typespace");
+const typelast = document.querySelector("#typelast");
+const typereturn = document.querySelector("#typereturn");
 // let randomSound = Math.floor(Math.random() * 2);
 
 loop();
 
 function loop() {
-  if (counter < original.length) {
+  if (counter <= original.length) {
+    playSound();
     document.querySelector("#typewriter").textContent = original.slice(0, counter);
     counter++;
-    playSound();
-    setTimeout(loop, 500);
+    setTimeout(loop, 1500);
+    console.log(counter);
   }
 }
 
 function playSound() {
-  typesound1.play();
+  if (original[counter - 1] === " ") {
+    spacesound.play();
+    console.log(counter);
+  } else {
+    typesound1.play();
+  }
 }
